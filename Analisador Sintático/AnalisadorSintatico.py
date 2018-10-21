c9a97b381532546d8cff81e9ab89acd463ef1d01
jalersamuelzago <jalersamuelzago@hotmail.com>
2018-10-20 22:43:47 -0300

    Update AnalisadorSintatico.py

--- Analisador Sintático/AnalisadorSintatico.py
+++ Analisador Sintático/AnalisadorSintatico.py
@@ -7,7 +7,7 @@ def variavel(txt):
     return 1
 
 def variavel_f(txt):
-    letra = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+    letra = ['b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
     if txt[0] in letra:
         return variavel_f(txt[1:])
     if txt[0] == '':
