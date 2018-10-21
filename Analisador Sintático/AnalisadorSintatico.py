67fcd37ca0190bd31f87df037db27a3958c9254e
R-Escher <38119826+R-Escher@users.noreply.github.com>
2018-10-20 22:37:31 -0300

    Add files via upload

---
+++ Analisador Sintático/AnalisadorSintatico.py
@@ -0,0 +1,16 @@
+def variavel(txt):
+    letra = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+    if txt[0] in letra:
+        return variavel_f(txt[1:])
+    if txt[0] == '':
+        return 0
+    return 1
+
+def variavel_f(txt):
+    letra = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+    if txt[0] in letra:
+        return variavel_f(txt[1:])
+    if txt[0] == '':
+        return 0
+    return 1
+    
